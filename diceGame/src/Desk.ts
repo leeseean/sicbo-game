55c6604de43bfacde0b86d19637e4a4388640da8
qiuzhengchen <leeseean@qq.com>
2020-06-18 08:04:43 +0800

    调坐标啊，体力活

--- diceGame/src/Desk.ts
+++ diceGame/src/Desk.ts
@@ -4,7 +4,7 @@ class Desk extends egret.DisplayObjectContainer {
         this.addEventListener(egret.Event.ADDED_TO_STAGE, this.onAddToStage, this)
     }
     private onAddToStage() {
-        console.log(this.stage)
+        console.log(this)
         this.drawDesk()
     }
     private drawDesk() {
@@ -18,13 +18,16 @@ class Desk extends egret.DisplayObjectContainer {
         // desk.addEventListener(mouse.MouseEvent.ROLL_OVER, (e) => console.log(e), this);
         // desk.addEventListener(mouse.MouseEvent.MOUSE_MOVE, (e) => console.log(e), this);
         // desk.addEventListener(mouse.MouseEvent.ROLL_OUT, (e) => console.log(e), this);
-        desk.addEventListener(egret.TouchEvent.TOUCH_MOVE, (e) => console.log(e), this);
-        desk.addEventListener(egret.TouchEvent.TOUCH_TAP, (e) => console.log(e), this);
+        // desk.addEventListener(egret.TouchEvent.TOUCH_MOVE, (e) => console.log(e), this);
+        // desk.addEventListener(egret.TouchEvent.TOUCH_TAP, (e) => console.log(e), this);
         // desk.addEventListener(mouse.MouseEvent.MOUSE_OUT, (e) => console.log(e), this);
-        const rect = Utils.drawRect(0x0000ff, [250, 240, 250, 150]);
-        console.log(rect)
-        const roundRect = Utils.drawRoundRect(0x0000ff, [400, 400, 100, 100, 20, 20]);
-        this.addChild(rect);
-        this.addChild(roundRect);
+        console.log(SicboConfig)
+        for (let key in SicboConfig) {
+            if (SicboConfig[key].length === 0) {
+                continue;
+            }
+            const roundRect = Utils.drawRoundRect(0x0000ff, SicboConfig[key]);
+            this.addChild(roundRect);
+        }
     }
 }
